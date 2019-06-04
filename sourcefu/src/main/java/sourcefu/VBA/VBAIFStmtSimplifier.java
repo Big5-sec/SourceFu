2c6ace366db8a491c4988fbe4e4f74f39b64a9bf
robin <nicolas.zilio@hotmail.fr>
2019-06-05 01:25:33 +0200

    reviewed if stmt simplifier for VBA

--- sourcefu/src/main/java/sourcefu/VBA/VBAIFStmtSimplifier.java
+++ sourcefu/src/main/java/sourcefu/VBA/VBAIFStmtSimplifier.java
@@ -1,15 +1,22 @@
 package sourcefu.VBA;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.TokenStreamRewriter;
 
 import sourcefu.VBA.antlr.VBAParser;
 import sourcefu.VBA.antlr.VBAParserBaseListener;
+import sourcefu.VBA.partialevaluator.RewriteOperation;
 
 public class VBAIFStmtSimplifier extends VBAParserBaseListener {
 	
 	TokenStreamRewriter rewriter;
 	Integer numberModifications=0;
+	Map<Token, RewriteOperation> operations = new LinkedHashMap<Token,RewriteOperation>();
 	
 	public VBAIFStmtSimplifier(CommonTokenStream tokens) {
 		rewriter=new TokenStreamRewriter(tokens);
@@ -50,39 +57,71 @@ public class VBAIFStmtSimplifier extends VBAParserBaseListener {
 	
 	//then we treat the case of the IfStmt
 	public void exitIfStmt(VBAParser.IfStmtContext ctx) {
-	    //System.out.println("in if stmt");
+		Boolean evaluable = true;
 		if(ctx.booleanExpression().getText().equals("True")) {
-			this.rewriter.replace(ctx.start, ctx.stop, ctx.block().getText());
+			RewriteOperation op = new RewriteOperation(ctx.start, ctx.stop, ctx.block().getText());
+			operations.put(ctx.start, op);
+			//this.rewriter.replace(ctx.start, ctx.stop, ctx.block().getText());
 			this.numberModifications+=1;
 			return;
 		} else if (ctx.booleanExpression().getText().equals("False")) {
-			//first we check every possible elseif block
-			if (ctx.elseIfBlock()!=null) {
-				int nbelseif = ctx.elseIfBlock().size();
-				for(int i =0; i< nbelseif; i++) {
-					if(ctx.elseIfBlock(i).booleanExpression().getText().equals("True")) {
-						this.rewriter.replace(ctx.start, ctx.stop, ctx.elseIfBlock(i).block().getText());
+			RewriteOperation op = new RewriteOperation(ctx.start, ctx.block().stop, "");
+			operations.put(ctx.start, op);
+			//this.rewriter.replace(ctx.start, ctx.block().stop, "");
+			this.numberModifications+=1;
+		} else {
+			evaluable = false;
+		}
+		
+		
+		if (ctx.elseIfBlock()!=null) {
+			int nbelseif = ctx.elseIfBlock().size();
+			Boolean first = true;
+			for(int i =0; i< nbelseif; i++) {
+				if(ctx.elseIfBlock(i).booleanExpression().getText().equals("True")) {
+					//this.rewriter.replace(ctx.start, ctx.stop, ctx.elseIfBlock(i).block().getText());
+					if(evaluable) {
+						RewriteOperation op = new RewriteOperation(ctx.start, ctx.stop, ctx.elseIfBlock(i).block().getText());
+						operations.put(ctx.start, op);
 						this.numberModifications+=1;
 						return;
-					} else if (ctx.elseIfBlock(i).booleanExpression().getText().equals("False")) {
-						continue;
-					} else {
-						//that means at least one expression has not been evaluated, so we should stop here
-						return;
 					}
+				} else if (ctx.elseIfBlock(i).booleanExpression().getText().equals("False")) {
+					this.rewriter.replace(ctx.elseIfBlock(i).start, ctx.elseIfBlock(i).stop, "");
+					this.numberModifications+=1;
+					//continue;
+				} else {
+					evaluable = false;
+					if (first) {
+						//this.rewriter.replace(ctx.elseIfBlock(i).ELSEIF().getSymbol().getStartIndex(), ctx.elseIfBlock(i).ELSEIF().getSymbol().getStopIndex(), "If");
+						RewriteOperation op = new RewriteOperation(ctx.elseIfBlock(i).ELSEIF().getSymbol(), ctx.elseIfBlock(i).ELSEIF().getSymbol(), "If");
+						operations.put(ctx.elseIfBlock(i).ELSEIF().getSymbol(), op);
+						first = false;
+					}
+					//that means at least one expression has not been evaluated, so we should stop here
+					//return;
 				}
 			}
-			
-			//if there, then if else block exists, it means this is the true one, so replace by its text
-			if (ctx.elseBlock()!=null) {
-				this.rewriter.replace(ctx.start, ctx.stop, ctx.elseBlock().block().getText());
-				this.numberModifications+=1;
-				return;
-			}
-			
-			//everything is false here
-			this.rewriter.replace(ctx.start, ctx.stop, "");
+		}
+		
+		//if there, then if else block exists, it means this is the true one, so replace by its text
+		if ((ctx.elseBlock()!=null) && (evaluable)) {
+			RewriteOperation op = new RewriteOperation(ctx.start, ctx.stop, ctx.elseBlock().block().getText());
+			operations.put(ctx.start, op);
+			//this.rewriter.replace(ctx.start, ctx.stop, ctx.elseBlock().block().getText());
 			this.numberModifications+=1;
+			return;
+		}
+		
+		
+	}
+	
+	public void applyModifications() {
+		for(Entry<Token, RewriteOperation> entry: this.operations.entrySet()) {
+//			System.out.print("entry : ");
+//			System.out.println(entry.getValue().start);
+//			System.out.println("replace:" + entry.getValue().replacement);
+			rewriter.replace(entry.getValue().start, entry.getValue().stop, entry.getValue().replacement);
 		}
 	}
 	
@@ -91,6 +130,7 @@ public class VBAIFStmtSimplifier extends VBAParserBaseListener {
 	}
 	
 	public String getdata() {
+		applyModifications();
 		return rewriter.getText();
 	}
 	
